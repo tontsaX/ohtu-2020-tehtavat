fbc9a9f11986ae03d0d11dad19fc92ebb8506f5b
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2020-03-09 16:49:56 +0200

    Parts 1 and 2

--- week3/nhlreader/src/main/java/ohtu/Main.java
+++ week3/nhlreader/src/main/java/ohtu/Main.java
@@ -5,10 +5,38 @@
  */
 package ohtu;
 
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.apache.http.client.fluent.Request;
+
 /**
  *
  * @author mluukkai
  */
 public class Main {
-    
+
+	public static void main(String[] args) throws IOException {
+		String url = "https://nhlstatisticsforohtu.herokuapp.com/players";		
+		String json = Request.Get(url).execute().returnContent().asString();
+
+		Gson gson = new Gson();
+		Player[] players = gson.fromJson(json, Player[].class);
+		
+		ArrayList<Player> finPlayers = new ArrayList<>();
+		
+		for(Player player: players) {
+			if(player.getNationality().equalsIgnoreCase("fin")) {
+				finPlayers.add(player);
+			}
+		}
+		
+		Collections.sort(finPlayers, Player.PlayerPoints);
+		for(Player player: finPlayers) {
+			System.out.println(player.toString());
+		}
+
+	}
 }
--- week3/nhlreader/src/main/java/ohtu/Player.java
+++ week3/nhlreader/src/main/java/ohtu/Player.java
@@ -1,8 +1,11 @@
 
 package ohtu;
 
+import java.util.Comparator;
+
 public class Player {
-    private String name;
+    private String name, team, nationality, birthdate;
+    private int goals, assists, penalties;
 
     public void setName(String name) {
         this.name = name;
@@ -12,9 +15,78 @@ public class Player {
         return name;
     }
 
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	public String getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public int getAssists() {
+		return assists;
+	}
+
+	public void setAssists(int assists) {
+		this.assists = assists;
+	}
+
+	public int getGoals() {
+		return goals;
+	}
+
+	public void setGoals(int goals) {
+		this.goals = goals;
+	}
+
+	public int getPenalties() {
+		return penalties;
+	}
+
+	public void setPenalties(int penalties) {
+		this.penalties = penalties;
+	}
+	
+	public int points() {
+		return this.goals + this.assists;
+	}
+    
     @Override
     public String toString() {
-        return name;
+        return name + " "
+        		+ team + " "
+        		+ goals + " + "
+        		+ assists + " = "
+        		+ points();
     }
-      
+    
+    // sorts a collection of players by their points
+    // Comparator käytössä, koska näin voidaan lisätä enemmän tapoja, joilla lajitella pelaajakokoelma
+    public static Comparator<Player> PlayerPoints = new Comparator<Player>() {
+    	public int compare(Player p1, Player p2) {
+    		
+    		// kasvava järjestys
+    		//return p1.points()-p2.points();
+    		
+    		// laskeva järjestys
+    		return p2.points()-p1.points();
+    	}
+    };
 }
