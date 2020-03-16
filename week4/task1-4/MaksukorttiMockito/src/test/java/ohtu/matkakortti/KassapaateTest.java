0c2649a5075df7c3c32693fef7c2425443b4fe95
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2020-03-16 15:15:11 +0200

    Task 2 lataa tests

--- week4/task1-4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
+++ week4/task1-4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -40,5 +40,17 @@ public class KassapaateTest {
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
+    
+    @Test
+    public void lataaKortilleRahaaJosSummaPositiivinen() {
+    	kassa.lataa(kortti, 5);
+    	verify(kortti).lataa(eq(5));
+    }
+    
+    @Test
+    public void kortilleEiLadataRahaaJosSummaNegatiivinen() {
+    	kassa.lataa(kortti, -5);
+    	verify(kortti, times(0)).lataa(anyInt());
+    }
       
 }
