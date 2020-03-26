cdb5d0cbc61bbf90799e394a178551e861fbbe9b
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2020-03-26 14:10:22 +0200

    Oikeassa paikassa ja ohjelma ei toimi

--- week5/tasks4-5/LaskinFX11/src/main/java/laskin/Nollaus.java
+++ week5/tasks4-5/LaskinFX11/src/main/java/laskin/Nollaus.java
@@ -3,9 +3,9 @@ package laskin;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class Nollaa extends Komento {
+public class Nollaus extends Komento {
 
-	public Nollaa(TextField tuloskentta, TextField syotekentta, Button plus, Button miinus, Button nollaa,
+	public Nollaus(TextField tuloskentta, TextField syotekentta, Button plus, Button miinus, Button nollaa,
 			Button undo) {
 		super(tuloskentta, syotekentta, plus, miinus, nollaa, undo);
 	}
--- week5/tasks4-5/LaskinFX11/src/main/java/laskin/Tapahtumankuuntelija.java
+++ week5/tasks4-5/LaskinFX11/src/main/java/laskin/Tapahtumankuuntelija.java
@@ -1,59 +1,28 @@
 package laskin;
 
+import java.util.HashMap;
+
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Tapahtumankuuntelija implements EventHandler {
-    private TextField tuloskentta; 
-    private TextField syotekentta; 
-    private Button plus;
-    private Button miinus;
-    private Button nollaa;
-    private Button undo;
-    private Sovelluslogiikka sovellus;
+    
+	private HashMap<Button, Komento> komennot = new HashMap<>();
 
-    public Tapahtumankuuntelija(TextField tuloskentta, TextField syotekentta, Button plus, Button miinus, Button nollaa, Button undo) {
-        this.tuloskentta = tuloskentta;
-        this.syotekentta = syotekentta;
-        this.plus = plus;
-        this.miinus = miinus;
-        this.nollaa = nollaa;
-        this.undo = undo;
-        this.sovellus = new Sovelluslogiikka();
+    public Tapahtumankuuntelija(TextField tuloskentta, TextField syotekentta, Button ynnaa, Button vahenna, Button nollaa, Button undo) {
+    	undo.disableProperty().set(false);
+    	komennot.put(ynnaa, new Summa(tuloskentta, syotekentta, ynnaa, vahenna, nollaa, undo));
+    	komennot.put(vahenna, new Erotus(tuloskentta, syotekentta, ynnaa, vahenna, nollaa, undo));
+    	komennot.put(nollaa, new Nollaus(tuloskentta, syotekentta, ynnaa, vahenna, nollaa, undo));
+    	komennot.put(undo, new Undo(tuloskentta, syotekentta, ynnaa, vahenna, nollaa, undo));
     }
     
     @Override
     public void handle(Event event) {
-        int arvo = 0;
- 
-        try {
-            arvo = Integer.parseInt(syotekentta.getText());
-        } catch (Exception e) {
-        }
- 
-        if (event.getTarget() == plus) {
-            sovellus.plus(arvo);
-        } else if (event.getTarget() == miinus) {
-            sovellus.miinus(arvo);
-        } else if (event.getTarget() == nollaa) {
-            sovellus.nollaa();
-        } else {
-            System.out.println("undo pressed");
-        }
-        
-        int laskunTulos = sovellus.tulos();
-        
-        syotekentta.setText("");
-        tuloskentta.setText("" + laskunTulos);
-        
-        if ( laskunTulos==0) {
-            nollaa.disableProperty().set(true);
-        } else {
-            nollaa.disableProperty().set(false);
-        }
-        undo.disableProperty().set(false);
+    	
+        komennot.get(event.getTarget()).suorita();
     }
 
 }
